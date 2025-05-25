4b0678dd1af3450d8be907a1466945c8f4878957
rohit jain <rohitjain97130@gmail.com>
2025-05-25 21:00:32 +0530

    changes done

---
+++ node-js-inbuild/path.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+
+//I want to get extension of the file
+
+//f1.txt , vedio.mp4 , song.mp3
+
+const extensionname = path.extname('C:\scaler course\LLD-4\node-js-inbuild\MyDirectory1\1.js');
+
+console.log(extensionname);
+
+
---
+++ node-js-inbuild/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+//exitsSync - it help  whether the file or directory exist 
+
+const fs= require('fs');
+// let ans = fs.existsSync('MyDirectory1')
+// let ans = fs.existsSync('1_fs.txt')
+let ans = fs.existsSync('MyDirectory1/1.js');
+console.log(ans);
