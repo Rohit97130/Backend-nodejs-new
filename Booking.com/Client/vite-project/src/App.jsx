46e97aa8d30e71b13a1b3d46d0288933fa8f6845
rohit jain <rohitjain97130@gmail.com>
2025-07-26 00:13:28 +0530

    change

--- Booking.com/Client/vite-project/src/App.jsx
+++ Booking.com/Client/vite-project/src/App.jsx
@@ -6,12 +6,15 @@ import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import ProtectedRoute from './components/ProtectedRoute';
+import {useSelector} from 'react-redux';
 
 function App() {
 
+   const {loading} = useSelector((state)=> state.loader)
 
   return (
     <>
+       {loading && <div className='loader-container'><div className='loader'></div></div>}
        <BrowserRouter>
         <Routes>
            <Route  path='/' element = {<ProtectedRoute><Home/></ProtectedRoute>}/>
--- Booking.com/Client/vite-project/src/components/ProtectedRoute.jsx
+++ Booking.com/Client/vite-project/src/components/ProtectedRoute.jsx
@@ -2,19 +2,41 @@
 import React, { useEffect, useState } from "react";
 import { GetCurrentUser } from "../apicalls/users";
 import { useNavigate } from "react-router-dom";
+import { showLoading, hideLoading} from "../redux/loaderSlice"
+import { useDispatch } from "react-redux";
+import {Layout ,Menu}  from "antd"
+const { Header, Footer, Sider, Content } = Layout;
+import { HomeOutlined , UserOutlined } from '@ant-design/icons';
 
 function ProtectedRoute({ children }) {
   // random checks
+ const navItems = [
+   {
+    label: 'Home',
+    icon : <HomeOutlined />
+   },
+   {
+     label: 'Profile',
+     icon: <UserOutlined />
+   }
+ ]
+
+ 
+
  const [user , setUser] = useState(null);
 
+   const dispatchEvent = useDispatch();
 
   //random check
   const getValidUser = async () => {
     try {
+      //show Loader
+      dispatchEvent(showLoading())
       const response = await GetCurrentUser();
       setUser(response.data); 
       console.log(user);
-      
+      dispatchEvent(hideLoading());
+      //hide Loader
       console.log("rrrr" , response);
     } catch (error) {
       console.log(error);
@@ -32,7 +54,27 @@ function ProtectedRoute({ children }) {
     }
   }, []);
 
-  return <div> {user && user.name} {children}</div>;
+
+  
+  return <div> 
+     <>
+      <Layout>
+      <Header style={{
+        backgroundColor: '#001529',
+        color: 'white',
+        position: 'sticky',
+        top: 0,
+        zIndex: 1,
+        width: "100%",
+        display: 'flex',
+        alignItems: 'center',
+      }}>
+        <h3 className="demo-logo text-white m-0" style={{color:"white"}}> Book my Show</h3>
+        <Menu theme="dark" mode="horizontal" items={navItems}/> 
+        </Header>
+    </Layout>
+     </>
+  </div>;
 }
 
 export default ProtectedRoute;
